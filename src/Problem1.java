4a872e8d6376b9c452a6729ee44a0d97c0c7e3f6
Matthewh1225 <mhuckman97@yahoo.com>
2024-11-16 05:00:20 -0600

    final commit

--- src/Problem1.java
+++ src/Problem1.java
@@ -1,62 +1,45 @@
 public class Problem1 {
     public static String search(int[][] matrix,int valueToFind){
-        int row = matrix.length;
-
-        int [] row0 = matrix[0];
-        int [] row1 = matrix[1];
-        int [] row2 = matrix[2];
-        int [] row3 = matrix[3];
-        int [] row4 = matrix[4];
-        int [] row5 = matrix[5];
-
-        for (int i=0; i < row ;i++){ 
-            if(row0[i]==valueToFind){
-                return "0,"+i;
-            }
-            else if(row1[i]==valueToFind){
-                return "1,"+i;
-            }
-            else if(row2[i]==valueToFind){
-                return "2,"+i;
-            }
-            else if(row3[i]==valueToFind){
-                return "3,"+i;
-            }
-            else if(row4[i]==valueToFind){
-                return "4,"+i;
-            }
-            else if(row5[i]==valueToFind){
-                return "5,"+i;
-            }
-        }
-        return null;
-    }
-
-    public static String otherSearch(int[][] matrix,int valueToFind){
-        int column = matrix[0].length;
-        int row = matrix.length;
-       
-        for (int i = 0; i < row ;i++){
-            for (int j = 0; j < column ;j++){
+        for (int i = 0; i < matrix.length ;i++){
+            for (int j = 0; j < matrix[i].length ;j++){
                 if (matrix[i][j]  == valueToFind){
                     return i+","+j;
                 }
             }   
         }
-        return null;
+        return null;   
     }
-               
-    public static void main(String[] args) throws Exception {
-        int[][] cat =  {{0,6,9,22,25,47},
-                       {3,11,19,27,45,49},
-                       {13,18,29,42,52,69},
-                       {16,31,51,55,67,75},
-                       {34,38,57,65,77,89},
-                       {61,62,82,85,97,98}};
 
-        System.out.println(search(cat,57));
+    public static String otherSearch(int[][] matrix,int valueToFind){
+        int row = matrix.length;
+        int colSize = matrix[0].length;
+
+        for (int i = 0; i < row ;i++){
+            int lastCell = matrix[i][colSize-1]; 
+            if(lastCell>=valueToFind){
+                int col = colSize-1;
+                while(col >=0 && matrix[i][col]!=valueToFind){
+                    col--;
+                }
+                if (col>=0){
+                    return  i+","+col;
+                }   
+            }
+        }
+                return null;
+    }
+            
+    public static void main(String[] args) throws Exception {
+        final int[][] CAT =  {{0,6,9,22,25,47},
+                             {3,11,19,27,45,49},
+                             {13,18,29,42,52,69},
+                             {16,31,51,55,67,75},
+                             {34,38,57,65,77,89},
+                             {61,62,82,85,97,98}};
+
+        System.out.println(search(CAT,57));
         System.out.println("\n");
-        System.out.println(otherSearch(cat,57));
+        System.out.println(otherSearch(CAT,57));
         
     }
 }
--- src/Problem2.java
+++ src/Problem2.java
@@ -1,162 +1,65 @@
-import java.util.*;
-
 public class Problem2 {
-
-    public static int moveRight(int x){
-        x=x+1;
-        return x;
-    }
-
-    public static int moveDown(int x){
-        x=x+1;
-        return x;
-    }
-    public static int moveLeft(int x){
-
-        x=x-1;
-        return x;
-    }
-    static void high(){
-        System.out.println("wowow");
-        
-    }
-    public static List<String> mazeSolver(int[][] maze){
-       
-        final int[] START = {0,0};
-        final int [] END ={8,8};
-     
-        boolean back = false;
-
-        int rowCount=0;
-        int colCount=0;
-
-        int[] splitPoint= { 0,0};
-
-        int[] currentPosition = {rowCount,colCount};
-       
-        int currentValue = maze[rowCount][colCount];
-        
-        int neighbors=0;
-
-        int currentCol= currentPosition[1];
-        int currentRow = currentPosition[0];
-       
-
-        int rightValue = maze[currentRow][currentCol+1];
-        int leftValue=0;
-        if ( currentCol==0) {
-            leftValue=1;
+    public static void mazeSolver(int[][] maze,int x, int y){
+        maze[x][y] = 8;
+        if(x == 8 && y == 8){
+            prinMaze(maze); 
+            maze[x][y] = 0;
+            return;
         }
-        else{leftValue=maze[currentRow][currentCol-1];
-
+        
+        int[] right= {x,y+1};
+        int[] left={x,y-1};
+        int[] up={x-1,y};
+        int[] down= {x+1,y};
+        int[][] possibleMoves={right,down,left,up};
+        for(int[] move:possibleMoves){
+            if(validity(maze, move[0], move[1])){
+                mazeSolver(maze, move[0], move[1]);
+            }
         }
-      
-        int downValue=maze[currentRow+1][currentCol];
-    
-        int[] rightPosition =  {currentRow,currentCol+1};
-        int[] downPosition = {currentRow+1,currentCol};
-        int[] leftPosition ={currentRow,currentCol-1};
-        if (leftPosition[1]<0){
+        maze[x][y]=0;
+    }
+    public static void prinMaze(int [][] maze) {
+        System.out.println("Soulution found!\n");
+        for (int[] row : maze) {
+            for (int col : row) {
+                String s = " "; 
+                if(col==1){
 
-            leftPosition[1]=0;
+                    s = "▒"; 
+                }
+                if (col==8){
+
+                    s= "█";
+                }
+                System.out.print(s);
+            }
+
+            System.out.println();
         }
-
-
-        List<String> path1 = new ArrayList<>();
-
-        // current=last
-        // next=current
-        
-        int savedSteps=0;
-        int step=0;
-        Boolean split=false;
-        
-        while(currentPosition != END){
-           
-            
-            
-            if ( currentCol==0) {
-                leftValue=1;
-            }
-            else{leftValue=maze[currentRow][currentCol-1];
-    
-            }
-            currentPosition[0]=currentRow;
-            currentPosition[1]=currentCol;
-            downValue=maze[currentRow+1][currentCol];
-            downPosition[0] = currentRow-1;
-            rightValue = maze[currentRow][currentCol+1];
-            
-
-            path1.add(Arrays.toString(currentPosition));
-            step++;
-
-            if (currentValue==1) {
-                return null;
-                
-            }
-            
-            else if(rightValue == 0 && downValue == 0){
-               if (lastPosition==path1.get(step))
-                if(!split){
-                    splitPoint[1]= currentCol;
-                    splitPoint[0]=currentRow;
-                    savedSteps=step;
-                
-                    currentCol++;
-                }
-                else{
-                    currentRow++;
-                }
-                
-               
-            
-                
-    
-            }
-            else if (rightValue==1 && downValue ==0 ){
-                currentRow++;
-             
-                
-
-            }
-        
-            else if (rightValue==1 && leftValue==1 && downValue==1){
-                currentPosition=splitPoint;
-                currentCol=splitPoint[1];
-                currentRow=splitPoint[0];
-
-                for (int i = path1.size()-1; i > savedSteps-2 ;i--){ 
-                    path1.remove(i);
-                }
-                split=true;
-                
-                
-            }
-            else if (rightValue==0){
-                currentCol++;
-            }
-            else if (leftValue == 0){
-               currentCol--;
-            } 
-           
-        }  
-        return null;
-        
-}
+        System.out.println();
+    }
+    public static boolean validity (int[][] maze, int x,int y) {
+        if(maze.length==0 || x >= maze.length || x<0 || y<0 || y >= maze[x].length) {
+            return false;
+        }
+        return maze[x][y]==0; 
+    }
+ 
     public static void main(String[] args) {
         int[][] maze = {{0,0,1,1,1,1,1,1,1},
                         {1,0,0,0,0,0,0,0,1},
                         {1,0,1,1,0,1,1,0,1},
                         {1,0,1,0,0,1,0,0,1},
-                        {1,0,1,0,1,0,1,0,1},
+                        {1,0,1,0,1,1,1,0,1},
                         {1,0,0,0,0,0,1,0,1},
                         {1,1,0,1,1,0,1,1,1},
                         {1,0,0,0,0,0,0,0,0},
                         {1,1,1,1,1,1,1,1,0}}; 
 
-        System.out.println(mazeSolver(maze));
-        high();
+            mazeSolver(maze, 0, 0);    
     }
 } 
+
+
      
